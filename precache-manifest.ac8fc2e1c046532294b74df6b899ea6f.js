ab69e2dfb177ae89072f1fb930f93ab017e99953
rlisboars <rlisboars@gmail.com>
2019-05-01 13:01:55 -0300

    Updates

---
+++ precache-manifest.ac8fc2e1c046532294b74df6b899ea6f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e2bff86c5a0b28068536752af393e6",
+    "url": "/react-gitcompare/index.html"
+  },
+  {
+    "revision": "ce2731cba8c9b20a4855",
+    "url": "/react-gitcompare/static/css/2.86cab6d9.chunk.css"
+  },
+  {
+    "revision": "ce2731cba8c9b20a4855",
+    "url": "/react-gitcompare/static/js/2.44ec8eb7.chunk.js"
+  },
+  {
+    "revision": "a96a0f711da823092009",
+    "url": "/react-gitcompare/static/js/main.ea3ae6fd.chunk.js"
+  },
+  {
+    "revision": "bc5d558369311865db0b",
+    "url": "/react-gitcompare/static/js/runtime~main.d10ad948.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-gitcompare/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-gitcompare/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-gitcompare/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-gitcompare/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-gitcompare/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
